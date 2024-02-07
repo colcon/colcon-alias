5ec35d6e3ade29eb7627f1f722842480b2b33801
Scott K Logan <logans@cottsay.net>
2024-02-07 13:48:30 -0600

    Add markers to linter tests, move linter imports
    Test markers can be used to easily (de-)select tests, and colcon exposes
    mechanisms to do so. Linters are a category of tests that are commonly
    called out.
    
    Additionally, if we move the imports for some of our single-purpose
    tests into the test function, we can avoid installing the linter
    dependencies entirely. This is a common case in platform packaging, where
    linter errors are not actionable and the dependencies are not typically
    installed.

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -5,15 +5,21 @@ import logging
 from pathlib import Path
 import sys
 
-from flake8 import LOG
-from flake8.api.legacy import get_style_guide
+import pytest
 
 
-# avoid debug and info messages from flake8 internals
-LOG.setLevel(logging.WARN)
-
-
+@pytest.mark.flake8
+@pytest.mark.linter
 def test_flake8():
+    from flake8.api.legacy import get_style_guide
+
+    # avoid debug / info / warning messages from flake8 internals
+    logging.getLogger('flake8').setLevel(logging.ERROR)
+
+    # for some reason the pydocstyle logger changes to an effective level of 1
+    # set higher level to prevent the output to be flooded with debug messages
+    logging.getLogger('pydocstyle').setLevel(logging.WARNING)
+
     style_guide = get_style_guide(
         extend_ignore=['D100', 'D104'],
         show_source=True,
