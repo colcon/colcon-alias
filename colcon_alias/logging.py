1851435cf3e19830c9f69cc87f276b8b1c30abac
Scott K Logan <logans@cottsay.net>
2024-06-13 14:18:17 -0500

    fixup! Use newly added colcon_core.logging.get_effective_console_level
    I'd guess I should learn to use the APIs that I author...

--- colcon_alias/logging.py
+++ colcon_alias/logging.py
@@ -3,10 +3,11 @@
 
 import logging
 
+from colcon_core.logging import colcon_logger
 from colcon_core.logging import get_effective_console_level
 
 
 def configure_filelock_logger():
     """Configure the 'filelock' log level based on colcon's log level."""
-    log_level = get_effective_console_level()
+    log_level = get_effective_console_level(colcon_logger)
     logging.getLogger('filelock').setLevel(log_level)
